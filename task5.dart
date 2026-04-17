11a8d5ec280dd96a4fb9f8d5cfbe879ceabc3023
Klibar <ma8964032@gmail.com>
2026-04-17 20:59:07 +0200

    after feedback

--- task5.dart
+++ task5.dart
@@ -1,8 +1,8 @@
 // 5.Write a dart program to calculate the sum of natural numbers.
 void main() {
-  var x = 1;
+  var sum = 1;
   for (var i = 1; i <= 50; i++) {
-    x += x;
+    sum += sum;
   }
-  print(x);
+  print(sum);
 }
